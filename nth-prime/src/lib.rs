8ea6aeb003a6ecba24d69e86fee02a19a919c4ee
paul-leydier <paul.leydier@compass-ft.com>
2022-11-14 23:16:06 +0100

    nth prime exercise, naive implementation

---
+++ nth-prime/src/lib.rs
@@ -0,0 +1,20 @@
+pub fn nth(n: u32) -> u32 {
+    let mut counter = 0;
+    let mut guess = 2;
+    while counter < n {
+        guess += 1;
+        if is_prime(guess) {
+            counter+=1;
+        }
+    }
+    guess
+}
+
+fn is_prime(x: u32) -> bool {
+    for i in 2..x {
+        if x % i == 0 {
+            return false;
+        }
+    }
+    true
+}
