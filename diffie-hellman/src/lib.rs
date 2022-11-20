6feb280265fb0e818b7f1bbdcb0cd51f70caea1b
paul-leydier <paul.leydier@compass-ft.com>
2022-11-20 23:03:07 +0100

    diffie-hellman exercise

---
+++ diffie-hellman/src/lib.rs
@@ -0,0 +1,31 @@
+use rand::Rng;
+
+pub fn private_key(p: u64) -> u64 {
+    rand::thread_rng().gen_range(2..p)
+}
+
+pub fn public_key(p: u64, g: u64, a: u64) -> u64 {
+    modular_pow(g, a, p)
+}
+
+pub fn secret(p: u64, b_pub: u64, a: u64) -> u64 {
+    modular_pow(b_pub, a, p)
+}
+
+fn modular_pow(base: u64, exponent: u64, modulus: u64) -> u64 {
+    if modulus == 1 {
+        return 0;
+    }
+    let mut base = base;
+    let mut exponent = exponent;
+    let mut result = 1;
+    base %= modulus;
+    while exponent > 0 {
+        if exponent % 2 == 1 {
+            result = (result * base) % modulus;
+        }
+        exponent >>= 1;
+        base = (base * base) % modulus;
+    }
+    result
+}
